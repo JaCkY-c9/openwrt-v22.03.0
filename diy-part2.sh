b26ad24a38f560046be9da4048c76a632dea2766
jack987-cloud <60478985+jack987-cloud@users.noreply.github.com>
2021-09-09 23:49:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 wget https://downloads.openwrt.org/releases/21.02.0/targets/x86/64/config.buildinfo -O .config
+echo '
+ONFIG_TARGET_KERNEL_PARTSIZE=32
+CONFIG_TARGET_ROOTFS_PARTSIZE=800' >> ./target/linux/x86/config-5.4
